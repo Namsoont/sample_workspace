63c11ff34e7e2b332b6fbef804c352378b87488f
User <User@192.168.159.1>
2022-07-02 12:08:18 +0900

    commit

--- HelloWorld/src/co/edu/Example08.java
+++ HelloWorld/src/co/edu/Example08.java
@@ -11,15 +11,15 @@ public static void main(String[] args) {
 	System.out.print("1. 이름: ");
 	String name = scanner.nextLine();
 	
-	System.out.print("2. 주민번호 앞 6자리");
+	System.out.print("2. 주민번호 앞 6자리: ");
 	String ssn = scanner.nextLine();
 	
-	System.out.print("3. 전화번호");
+	System.out.print("3. 전화번호: ");
 	String tel = scanner.nextLine();
 	
 	System.out.println();
 	
-	System.out.println("[입력된 내용]");
+	System.out.println("[입력한 내용]");
 	System.out.println(name);
 	System.out.println(ssn);
 	System.out.println(tel);
