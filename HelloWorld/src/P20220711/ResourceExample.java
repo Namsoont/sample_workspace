da4a4b9fd1a16e5be25daa8efaf8fec0742a5690
admin <admin@YD-19>
2022-07-11 14:00:34 +0900

    a

--- HelloWorld/src/P20220711/ResourceExample.java
+++ HelloWorld/src/P20220711/ResourceExample.java
@@ -2,7 +2,7 @@ package P20220711;
 
 public class ResourceExample {
 	public static void main(String[] args) {
-		
+		//bin에 sample.txt파일
 		Class cls = ClassEx.class;
 		
 		String path = cls.getResource("sample.txt").getPath();
