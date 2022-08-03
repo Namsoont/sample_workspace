7e82fb3c0f79c2cb1ace3942d9e49af92b86d635
admin <admin@YD-19>
2022-08-03 13:08:44 +0900

    a

--- HelloWorld/src/P20220803/StringApI.java
+++ HelloWorld/src/P20220803/StringApI.java
@@ -2,9 +2,26 @@ package P20220803;
 
 public class StringApI {
 	public static void main(String[] args) {
-		
+		//100번지
 		String a = "aaa";
 		
-		String b = "bbb";
+		//"aaabbb" 101번지
+		a = a+"aaa";
+		
+		StringBuilder sb = new StringBuilder();
+		
+		//100번지
+		sb.append("aaa");
+		//100번지
+		sb.append("bb");
+		
+		
+//		String b = "bbb";
+//		
+//		String c = new String("aaa");
+//		
+//		if ( a==c ) //주소값비교 (객체값)
+//			
+//		if( a.equals(c)); // 안에있는 데이터 구분
 	}
 }
